b73178c339979d292daf6b88b394a4f7d23ae2e5
omerteomim <omerteomim@gmail.com>
2025-06-01 11:35:17 +0300

    trigger fixed

--- main.tf
+++ main.tf
@@ -64,10 +64,10 @@ resource "aws_lambda_function" "summarizer_lambda" {
 }
 
 #s3 trigger to the summarizer lambda
-resource "aws_s3_bucket_notification" "video_upload_trigger" {
+resource "aws_s3_bucket_notification" "video_summary_trigger" {
   bucket = aws_s3_bucket.videos_bucket.id
   lambda_function {
-    lambda_function_arn = aws_lambda_function.uploader_lambda.arn
+    lambda_function_arn = aws_lambda_function.summarizer_lambda.arn
     events              = ["s3:ObjectCreated:*"]
     filter_prefix       = "telegram_videos/"
     filter_suffix       = ".mp4"
@@ -77,7 +77,7 @@ resource "aws_s3_bucket_notification" "video_upload_trigger" {
 resource "aws_lambda_permission" "allow_s3_invoke" {
   statement_id  = "AllowS3Invoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.uploader_lambda.function_name
+  function_name = aws_lambda_function.summarizer_lambda.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = aws_s3_bucket.videos_bucket.arn
 }
